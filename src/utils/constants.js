25efe9e12313d4ed0f3021607641186201f2152c
KatsuroAlex <katsuroay@mail.ru>
2023-05-16 16:22:30 +0400

    test

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://api.katsuromovies-diplom.nomoredomains.monster';
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'https://api.katsuromovies-diplom.nomoredomains.monster';
+// const BASE_URL = 'http://localhost:3001';
 const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 
 export {
